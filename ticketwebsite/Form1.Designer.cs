131d78949112e4c7df030bcc04b5a8f7801f4df7
natme2105 <nathanielgutierrez70@gmail.com>
2023-11-27 20:06:46 +0800

    Initial commit

---
+++ ticketwebsite/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace ticketwebsite
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Cyan;
+            button1.FlatStyle = FlatStyle.System;
+            button1.Location = new Point(78, 36);
+            button1.Name = "button1";
+            button1.Size = new Size(209, 57);
+            button1.TabIndex = 0;
+            button1.Text = "Admin";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(304, 36);
+            button2.Name = "button2";
+            button2.Size = new Size(190, 57);
+            button2.TabIndex = 1;
+            button2.Text = "buy ticket";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(742, 36);
+            button3.Name = "button3";
+            button3.Size = new Size(204, 54);
+            button3.TabIndex = 2;
+            button3.Text = "Ticket outlet";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(527, 36);
+            button4.Name = "button4";
+            button4.Size = new Size(193, 54);
+            button4.TabIndex = 3;
+            button4.Text = "Event";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(1135, 564);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            Text = "Form1";
+            TransparencyKey = Color.White;
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+    }
+}
